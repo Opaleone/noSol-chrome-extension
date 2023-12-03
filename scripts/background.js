63849f499743a100e641eb0d13b8e145a82119c9
Leone Varando <lvarando@gmail.com>
2023-12-02 18:34:11 -0600

    Added function to sendMessage to popup

--- scripts/background.js
+++ scripts/background.js
@@ -1,13 +1,15 @@
 chrome.runtime.onMessage.addListener((req, sender, sendRes) => {
   if (req.greeting === 'Hello') {
-    const urlReg = /(http|https):\/\/leetcode.com\/problems\/[a-z-/]{1,256}/gi
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      const activeTab = tabs[0];
-      const tabUrl = activeTab.url;
-
-      console.log(activeTab);
-      console.log(tabUrl.match(urlReg));
-      sendRes({ farewell: 'Goodbye' })
-    })
+    sendUrl();
+    sendRes({ farewell: 'Goodbye' })
   }
 })
+
+const sendUrl = () => {  
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    const activeTab = tabs[0];
+    const tabUrl = activeTab.url;
+
+    chrome.runtime.sendMessage({ greeting: 'Hello', url: tabUrl })
+  })
+}
