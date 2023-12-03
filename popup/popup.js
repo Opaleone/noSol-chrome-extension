9fa805b1b9e1ca2772255be3aca8a1ad9c4c4352
Leone Varando <lvarando@gmail.com>
2023-12-02 18:35:09 -0600

    Added listener to popup js file to listen for url

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,19 @@
 const toggle = document.body.querySelector('.slider');
+chrome.runtime.sendMessage({ greeting: "Hello" });
+let response;
+const urlReg = /(http|https):\/\/leetcode.com\/problems\/[a-z-/]{1,256}/gi
+
+chrome.runtime.onMessage.addListener((req, sender, sendRes) => {
+  if (req.greeting === 'Hello') {
+    response = req.url;
+    sendRes({ farewell: 'Goodbye'})
+  }
+})
+
+setTimeout(() => {
+  console.log(response);
+}, 1000);
 
 toggle.addEventListener('click', async () => {
-  const response = await chrome.runtime.sendMessage({ greeting: "Hello" });
+  // doSomething();
 })
