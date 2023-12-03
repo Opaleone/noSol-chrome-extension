13f8955fb236c31bc12da976d890b3145db9f403
Leone Varando <lvarando@gmail.com>
2023-12-03 17:40:31 -0600

    Added location of solutions tab in leetcode as variable

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,6 @@
 (async () => {
   let getUrl = await chrome.runtime.sendMessage({ greeting: "Hello"});
+
+  const solutionsTab = document.body.querySelector("[data-layout-path='/ts0/tb2'");
+
 })();
