82070875d27a31398d568c68e1f1bfbb34dfebff
Leone Varando <lvarando@gmail.com>
2023-12-04 18:02:26 -0600

    Moved main extension functionality to content script

--- popup/popup.js
+++ popup/popup.js
@@ -1,20 +1,11 @@
 const toggle = document.body.querySelector('.slider');
-chrome.runtime.sendMessage({ greeting: "Hello" });
-let response;
-const urlReg = /(http|https):\/\/leetcode.com\/problems\/[a-z-/]{1,256}/gi
 
-chrome.runtime.onMessage.addListener((req, sender, sendRes) => {
-  if (req.greeting === 'Hello') {
-    if (req.url.match(urlReg)) response = true;
-    else response = false;
-    sendRes({ farewell: 'Goodbye'})
-  }
-})
-
-setTimeout(() => {
-  console.log(response);
-}, 1000);
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["../scripts/content.js"]
+  });
+});
 
 toggle.addEventListener('click', async () => {
-  // doSomething();
-})
+});
--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,24 @@
 (async () => {
   let getUrl = await chrome.runtime.sendMessage({ greeting: "Hello"});
+  const urlReg = /(http|https):\/\/leetcode.com\/problems\/[a-z-/]{1,256}/gi
+  let response;
+
+  chrome.runtime.sendMessage({ greeting: "Hello" });
+
+  chrome.runtime.onMessage.addListener((req, sender, sendRes) => {
+    if (req.greeting === 'Hello') {
+      if (req.url.match(urlReg)) response = true;
+      else response = false;
+      sendRes({ farewell: 'Goodbye'})
+    }
+  })
+
+  setTimeout(() => {
+    console.log(response);
+  }, 1000);
 
   const solutionsTab = document.body.querySelector("[data-layout-path='/ts0/tb2'");
 
+  console.log('ding');
+  console.log(solutionsTab);
 })();
