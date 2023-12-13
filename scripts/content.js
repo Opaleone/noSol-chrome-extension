d9700ff3811aa55934f0f45fee52c165d0f4c92a
Leone Varando <lvarando@gmail.com>
2023-12-12 23:47:35 -0600

    Trying other methods to get response

--- scripts/content.js
+++ scripts/content.js
@@ -2,11 +2,13 @@
   let getUrl = await chrome.runtime.sendMessage({ greeting: "Hello"});
   const urlReg = /(http|https):\/\/leetcode.com\/problems\/[a-z-/]{1,256}/gi
   let response;
+  let solutionsTab;
 
   chrome.runtime.sendMessage({ greeting: "Hello" });
 
   chrome.runtime.onMessage.addListener((req, sender, sendRes) => {
-    if (req.greeting === 'Hello') {
+    if (req.url) {
+      console.log('ding');
       if (req.url.match(urlReg)) response = true;
       else response = false;
       sendRes({ farewell: 'Goodbye'})
@@ -16,9 +18,7 @@
   setTimeout(() => {
     console.log(response);
   }, 1000);
-
-  const solutionsTab = document.body.querySelector("[data-layout-path='/ts0/tb2'");
-
-  console.log('ding');
+    
+  solutionsTab = document.body.querySelector("[data-layout-path='/ts0/tb2']");
   console.log(solutionsTab);
 })();
