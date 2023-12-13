6df6b96fc272018d72091f960f131560a9f94859
Leone Varando <lvarando@gmail.com>
2023-12-12 23:48:48 -0600

    Removed unnecessary code

--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,4 @@
 const toggle = document.body.querySelector('.slider');
 
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ["../scripts/content.js"]
-  });
-});
-
 toggle.addEventListener('click', async () => {
 });
