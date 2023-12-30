777250c3a4df5824310ea5e42e3df4a3803fe41d
Leone Varando <lvarando@gmail.com>
2023-12-30 17:56:40 -0600

    Successfully retrieved solutions tab and set display to none

--- scripts/content.js
+++ scripts/content.js
@@ -1,32 +1,17 @@
 (async () => {
-  let getUrl = await chrome.runtime.sendMessage({ greeting: "Hello"});
-  const urlReg = /(http|https):\/\/leetcode.com\/problems\/[a-z-/]{1,256}/gi
-  let response;
+  console.log('dong');
   let solutionsTab;
 
-  chrome.runtime.sendMessage({ greeting: "Hello" });
-
-  chrome.runtime.onMessage.addListener((req, sender, sendRes) => {
-    if (req.url) {
-      console.log('ding');
-      if (req.url.match(urlReg)) response = true;
-      else response = false;
-      sendRes({ farewell: 'Goodbye'})
-    }
-  })
-
-  setTimeout(() => {
-    console.log(response);
-  }, 1000);
-
   const myMain = (e) => {
-    console.log('ding');
     let jsInitCheckTimer = setInterval(checkForSol, 10)
+    function checkForSol(e) {
+      if (typeof solutionsTab !== 'undefined' || document.body.querySelector("[data-layout-path='/ts0/tb2']")) {
+        clearInterval(jsInitCheckTimer);
+        solutionsTab = document.body.querySelector("[data-layout-path='/ts0/tb2']");
+        console.log(solutionsTab);
 
-    if (typeof solutionsTab !== 'undefined' || document.body.querySelector("[data-layout-path='/ts0/tb2']")) {
-      clearInterval(jsInitCheckTimer);
-      solutionsTab = document.body.querySelector("[data-layout-path='/ts0/tb2']");
-      console.log(solutionsTab);
+        solutionsTab.style.display = "none";
+      }
     }
   }
 
