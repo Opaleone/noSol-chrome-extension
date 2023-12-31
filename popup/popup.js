8fcfacec7cab04a59732b546026e85217dfab592
Leone Varando <lvarando@gmail.com>
2023-12-30 18:14:29 -0600

    Added boolean storage to chrome storage

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,24 @@
 const toggle = document.body.querySelector('.slider');
+let storeToggle;
+chrome.storage.sync.get(['toggleStore'], function(items) {
+  storeToggle = items.toggleStore;
+});
 
 toggle.addEventListener('click', async () => {
+  console.log(storeToggle);
+
+  switch (storeToggle) {
+    case true:
+      storeToggle = false;
+      chrome.storage.sync.set({ 'toggleStore': false }, () => {
+        console.log('ToggleStore set to false');
+      })
+      break;
+    case false:
+      storeToggle = true;
+      chrome.storage.sync.set({ 'toggleStore': true }, () => {
+        console.log('ToggleStore set to true');
+      })
+      break;
+  }
 });
