b46a4af6b79c1c862ffe4ddacd99bfd9d279f2a2
Leone Varando <lvarando@gmail.com>
2024-09-29 15:32:38 -0500

    Removed unnecessary functions and console logs

--- scripts/content.js
+++ scripts/content.js
@@ -5,33 +5,18 @@
   let solutionsTab;
 
   chrome.storage.sync.set({ "toggleStore": true }, () => {
-    console.log('Settings Saved!');
+    return;
   })
   await chrome.storage.sync.get(null, async (res) => {
     timerLength = await res.timerLength;
     timerPower = await res.timerPower;
     storeToggle = await res.toggleStore;
-
-    console.log(parseInt(timerLength));
-    console.log(timerPower);
   })
-
-  const findSolutions = (element) => {
-    if (element.hasChildNodes()) {
-      element.childNodes.forEach(findSolutions)
-    } else if (element.nodeType === Text.TEXT_NODE && element.textContent === "Solutions") {
-      solutionsTab = element;
-      return;
-    }
-  }
-
-  findSolutions(document.body);
 
   const solCheck = (e) => {
     let jsInitCheckTimer = setInterval(checkForSol, 10);
     function checkForSol(e) {
       if (typeof solutionsTab !== 'undefined' || document.body.querySelector("[data-layout-path='/ts0/tb2']")) {
-        console.log("Solutions Removed!")
         clearInterval(jsInitCheckTimer);
         solutionsTab = document.body.querySelector("[data-layout-path='/ts0/tb2']");
         solutionsTab.style.display = "none";
