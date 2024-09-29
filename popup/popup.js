3b99458fa7713d7838fd5f7e35653c9609b29e76
Leone Varando <lvarando@gmail.com>
2024-09-29 17:25:41 -0500

    Removed console logs

--- popup/popup.js
+++ popup/popup.js
@@ -3,9 +3,7 @@ const slider = document.body.querySelector('#toggle-sol');
 let storeToggle;
 
 chrome.storage.sync.get(null, (res) => {
-  console.log(res);
   storeToggle = res.toggleStore;
-
   if (storeToggle) slider.checked = true;
   else slider.checked = false;
 })
@@ -14,15 +12,11 @@ toggle.addEventListener('click', async () => {
   switch (storeToggle) {
     case true:
       storeToggle = false;
-      chrome.storage.sync.set({ 'toggleStore': false }, () => {
-        console.log('ToggleStore set to false');
-      })
+      chrome.storage.sync.set({ 'toggleStore': false })
       break;
     case false:
       storeToggle = true;
-      chrome.storage.sync.set({ 'toggleStore': true }, () => {
-        console.log('ToggleStore set to true');
-      })
+      chrome.storage.sync.set({ 'toggleStore': true })
       break;
   }
 });
