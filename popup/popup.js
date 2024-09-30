040526fb81db656517a94ecba08ed07a378ffdce
Leone Varando <lvarando@gmail.com>
2024-09-30 06:45:27 -0500

    Added conditional for slider
    Added a conditional that checks if chrome storage value for disable cancellation is true and sets input disabled property to false if found

--- popup/popup.js
+++ popup/popup.js
@@ -1,14 +1,21 @@
 const toggle = document.body.querySelector('.slider');
 const slider = document.body.querySelector('#toggle-sol');
 let storeToggle;
+let cancelDisable;
 
 chrome.storage.sync.get(null, (res) => {
   storeToggle = res.toggleStore;
+  cancelDisable = res.cancelDisable;
+
   if (storeToggle) slider.checked = true;
   else slider.checked = false;
+
+  if (cancelDisable) slider.disabled = false;
 })
 
 toggle.addEventListener('click', async () => {
+  if (slider.disabled) return;
+
   switch (storeToggle) {
     case true:
       storeToggle = false;
